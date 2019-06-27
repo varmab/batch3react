8023beead55cd4f1265015c363cc0756ddcd99fe
Varma Bhupatiraju <varmab@gmail.com>
2019-06-27 15:47:29 +0530

    added Counter

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './Welcome'
+import Counter from './Counter'
+
+const Greeting=(props)=>{
+  return(
+    <h1>{props.children} Happy New Year, {props.year}</h1>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Welcome title="Varma" year={2020}/>
-          <Welcome title="Ram"/>
-          <Welcome title="Rani"/>
+          <Welcome title="Varma" year={2020} password="welcome"/>
+          <Welcome title="Ram" year={2018} password="12321321"/>
+          <Welcome year={2019} password="12321321"/>
         </header>
-        <input type="text"/>
+        <Greeting year={2020}>
+          ********
+        </Greeting>
+        <Counter/>
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class Welcome extends Component{
     constructor(){
@@ -17,9 +18,26 @@ class Welcome extends Component{
 
     render(){
         return (
-            <h1 onClick={this.updateClicks}>Welcome to {this.props.title} ({this.state.clicks})</h1>
+            <h1 onClick={this.updateClicks}>Welcome to {this.props.title} ({this.state.clicks}) - {this.props.year}</h1>
         )
     }
+}
+
+Welcome.propTypes={
+    title:PropTypes.string,
+    year:PropTypes.number.isRequired,
+    password(props, propName, component){
+        if(!(propName in props)){
+          return new Error(`missing ${propName}`)
+        }
+        if(props[propName].length < 6){
+          return new Error(`${propName} was too short`)
+        }
+    }
+}
+
+Welcome.defaultProps={
+    title:'Coding Sastra'
 }
 
 export default Welcome;
